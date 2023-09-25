758ce6dd86db1f98695cb2cc7c9635bfbb0de1b6
MigueliDeveloper <miguelidevelop@gmail.com>
2023-09-25 20:11:03 +0100

    HOLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr'
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,22 +16,22 @@ import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { CursoComponent } from './componentes/curso/curso.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      HeaderComponent,
-      ListarCursosComponent,
-      CrearCursosComponent,
-      // ContactoComponent,
-      CursoComponent
-   ],
+    HeaderComponent,
+    ListarCursosComponent,
+    CrearCursosComponent,
+    HomeComponent,
+    ContactoComponent,
+    CursoComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    // HttpClientModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    // NgbModule,
+    NgbModule,
     AppRoutingModule,
     ToastrModule.forRoot()
   ],
